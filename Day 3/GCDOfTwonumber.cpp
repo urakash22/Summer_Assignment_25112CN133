62e647a6cbf281a9f430be0a2aa1772e46fd86af
urakash22 <akashkumar798541@gmail.com>
2026-06-03 09:06:07 +0530

    Day 3

---
+++ Day 3/GCDOfTwonumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1, n2,rem;
+    cout<<"Enter the two number";
+    cin>>n1>>n2;
+    while(n1%n2!=0)
+    {
+       rem = n1%n2;
+        n1 = n2;
+        n2 = rem;
+    }
+    cout<<" GCD is "<<n2;
+    return 0;
+}
--- Day 3/primeNo.cpp
+++ Day 3/primeNo.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    
+
+    cout << "Enter the limit: ";
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        bool prime = true;
+
+        for (int j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime)
+            cout << i << " ";
+    }
+
+    return 0;
 }
