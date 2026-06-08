51cbaacc4727e438ddf8af2b3fa9c52f5e1f415c
urakash22 <akashkumar798541@gmail.com>
2026-06-08 22:27:57 +0530

    Dayn 8

---
+++ Day 8/characterTriangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(char ch = 'A'; ch < 'A' + i; ch++) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
